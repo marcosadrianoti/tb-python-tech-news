2ad6a39bcf3ed0f2f8637713be12aa4b3b04e20c
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-08-31 17:06:37 -0300

    Req 04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,8 +32,32 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author > a::text").get()
+    reading_time = int(
+        selector.css("li.meta-reading-time::text").re_first(r"\d+")
+    )
+    summary = (
+        selector.css("div.entry-content > p:first-of-type")
+        .xpath("string()")
+        .get()
+        .strip()
+    )
+    category = selector.css("a.category-style > span.label::text").get()
+    print(category)
+    new = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
+    return new
 
 
 # Requisito 5
