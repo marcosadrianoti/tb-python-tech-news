1058ac5e31b268977d133bb570dd18971480f1b9
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-09-01 10:53:09 -0300

    Req 08

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -15,8 +16,21 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        # https://blog.betrybe.com/desenvolvimento-web/dd-mm-aaaa-como-formatar-data/#2
+        # Primeiro tem que mudar o date de str para objeto datetime.
+        obj_date = datetime.strptime(date, "%Y-%m-%d")
+        new_date_format = obj_date.strftime("%d/%m/%Y")
+
+        regex = {"$regex": new_date_format}
+        query = {"timestamp": regex}
+        result_search_news = search_news(query)
+        news = []
+        for new in result_search_news:
+            news.append((new["title"], new["url"]))
+        return news
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
