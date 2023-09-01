da774656fcee245e129e494c23154dccc36f8b85
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-09-01 10:25:52 -0300

    Req 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+    regex = {"$regex": title, "$options": "i"}
+    query = {"title": regex}
+    result_search_news = search_news(query)
+    news = []
+    for new in result_search_news:
+        news.append((new["title"], new["url"]))
+    return news
 
 
 # Requisito 8
